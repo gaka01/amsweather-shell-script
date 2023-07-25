ddd0bfd4c1f1db05376265d5fc6ef8231e5b694f
Borislav Sapundzhiev <bsapundjiev@gmail.com>
2023-07-25 23:56:53 +0300

    Command provider is now async

--- gnome/amsweather@gnome.shell.org/dataProviders/commandDataProvider.js
+++ gnome/amsweather@gnome.shell.org/dataProviders/commandDataProvider.js
@@ -1,10 +1,42 @@
 'use strict';
 
 const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
 const ByteArray = imports.byteArray;
 
 class Command {
 
+    static ExcecuteAsync(cbResult, ...args) {
+        try {
+            let proc = Gio.Subprocess.new(
+                args, 
+                Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+            );
+            
+            proc.communicate_utf8_async(null, null, (proc, res) => {
+                var result = null;
+                try {
+                    let [, stdout, stderr] = proc.communicate_utf8_finish(res);
+
+                    if (proc.get_successful()) {
+                        result = stdout.trim(); 
+                    } else {
+                        log(stderr);
+                        result = null;
+                    }
+                } catch (e) {
+                    logError(e);
+                    result = null;
+                } finally {
+                    cbResult(result);
+                }
+            });
+        } catch (e) {
+            cbResult(null);
+        }
+    }
+  
+    
     static Excecute(...args) {
 
         let [res, out] = GLib.spawn_sync(null, args,
@@ -21,7 +53,6 @@ var Provider = class Provider {
     }
 
     provide(onNewData) {
-        const result = Command.Excecute(`${this._pathToExtension}/amsweather.sh`).trim();
-        onNewData(result);
+        Command.ExcecuteAsync((result) => onNewData(result), `${this._pathToExtension}/amsweather.sh`);
     }
 }
